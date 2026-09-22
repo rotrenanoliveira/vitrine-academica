87ca1e8299c25167df0eaf7aa1f4a12267f21484
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-09-22 15:37:21 -0300

    feat: implementing drizzle audit logs repository

---
+++ apps/api/src/infra/database/drizzle/schemas/audit-log.ts
@@ -0,0 +1,20 @@
+import { jsonb, pgEnum, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { sessions } from './sessions'
+
+export const auditLogActionEnum = pgEnum('audit_log_action_enum', ['CREATE', 'UPDATE', 'DELETE', 'LOGIN', 'LOGOUT'])
+
+export const auditLogStatusEnum = pgEnum('audit_log_status_enum', ['SUCCESS', 'FAILURE'])
+
+const auditLogsTable = pgTable('audit_logs', {
+  id: text().notNull().primaryKey(),
+  timestamp: timestamp().notNull(),
+  actorId: text('actor_id').notNull(),
+  sessionId: text('session_id').references(() => sessions.id),
+  action: auditLogActionEnum().notNull(),
+  resource: text().notNull(),
+  resourceId: text('resource_id').notNull(),
+  diff: jsonb().notNull(),
+  status: auditLogStatusEnum().notNull(),
+})
+
+export const auditLogs = auditLogsTable
